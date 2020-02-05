0a9b070f4112b45f56725f5e6c3641b0f899b2d2
Evgen904 <evgen904@gmail.com>
2020-02-06 00:27:26 +0300

    Сохранение модели

--- index.js
+++ index.js
@@ -15,3 +15,24 @@ mongoose.connect('mongodb://localhost/youtube-video', {
   .catch((error) => {
     console.log(error);
   })
+
+// регистрируем схему которую создали
+require('./person.model.js')
+
+const Person = mongoose.model('persons')
+
+// экземпляр класса Person
+const person = new Person({
+  name: 'Test text',
+  age: 24,
+  phones: [654654654]
+})
+
+// сохраняем модель в базу
+person.save()
+    .then((user) => {
+      console.log(user)
+    })
+    .catch(e => {
+      console.log(e)
+    })
